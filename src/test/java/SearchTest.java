19c85aae8c10144ef5d2d17a4dfad180d0dd7ad9
Amin Abu-Taleb <heavywizard@gmail.com>
2015-04-27 19:25:15 +0100

    Adding multiple-search test

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -36,11 +37,11 @@ public class SearchTest {
         String sample = "Bellingham";
 
         List<String> words = trie.search(sample);
+        Reporter.log("searchComplete: " + (System.currentTimeMillis() - start) + " millis", true );
 
         Assert.assertFalse(words.isEmpty());
         Assert.assertEquals(words.size(), 1);
         Assert.assertEquals(words.get(0), sample);
-        Reporter.log("searchComplete: " + (System.currentTimeMillis() - start) + " millis", true );
     }
 
     @Test(description = "Searches for the first half of one of the words")
@@ -49,12 +50,12 @@ public class SearchTest {
         String sample = "Bellin";
 
         List<String> words = trie.search(sample);
+        Reporter.log("searchFirstHalf: " + (System.currentTimeMillis() - start) + " millis", true );
 
         Assert.assertFalse(words.isEmpty());
         Assert.assertEquals(words.size(), 1);
         Assert.assertEquals(words.get(0), "Bellingham");
 
-        Reporter.log("searchFirstHalf: " + (System.currentTimeMillis() - start) + " millis", true );
     }
 
     @Test(description = "Searches for the first half of one of the words")
@@ -62,9 +63,28 @@ public class SearchTest {
         Long start = System.currentTimeMillis();
         String sample = "B";
         List<String> words = trie.search(sample);
+        Reporter.log("searchMultipleResults: " + (System.currentTimeMillis() - start) + " millis", true );
 
         Assert.assertFalse(words.isEmpty());
         Assert.assertEquals(words.size(), 35);
-        Reporter.log("searchMultipleResults: " + (System.currentTimeMillis() - start) + " millis", true );
+    }
+
+    @Test(description = "Searches for the first half of one of the words")
+    public void loadMultipleSearches(){
+        Long start = System.currentTimeMillis();
+        List<String> words1 = trie.search("Car");
+        List<String> words2 = trie.search("Clapham ");
+        List<String> words3 = trie.search("Sutton");
+        List<String> words4 = trie.search("Upper");
+        Reporter.log("loadMultipleSearches: " + (System.currentTimeMillis() - start) + " millis", true);
+
+        Assert.assertFalse(words1.isEmpty());
+        Assert.assertFalse(words2.isEmpty());
+        Assert.assertFalse(words3.isEmpty());
+        Assert.assertFalse(words4.isEmpty());
+        Assert.assertEquals(words1, Arrays.asList(new String[]{"Carpenders Park", "Carshalton", "Carshalton Beeches"}));
+        Assert.assertEquals(words2, Arrays.asList(new String[]{"Clapham High Street", "Clapham Junction"}));
+        Assert.assertEquals(words3, Arrays.asList(new String[]{"Sutton", "Sutton Common"}));
+        Assert.assertEquals(words4, Arrays.asList(new String[]{"Upper Holloway", "Upper Warlingham"}));
     }
 }
