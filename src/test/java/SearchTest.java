e59680a824ceddc095843897d057bda8d892c580
Amin Abu-Taleb <heavywizard@gmail.com>
2015-04-27 14:14:54 +0100

    Adding dummy test

---
+++ src/test/java/SearchTest.java
@@ -0,0 +1,12 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by taleba on 27/04/2015.
+ */
+public class SearchTest {
+    @Test
+    public void dummyTest(){
+        Assert.assertTrue(true);
+    }
+}
