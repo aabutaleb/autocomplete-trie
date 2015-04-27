80be00fa280a8d2a86789a004c8341741478e905
Amin Abu-Taleb <heavywizard@gmail.com>
2015-04-27 15:02:21 +0100

    Adding implementation

--- src/main/java/org/abutaleb/amin/ticketmachine/algorithm/Trie.java
+++ src/main/java/org/abutaleb/amin/ticketmachine/algorithm/Trie.java
@@ -1,7 +1,118 @@
 package org.abutaleb.amin.ticketmachine.algorithm;
 
+import java.util.*;
+
 /**
- * Created by taleba on 27/04/2015.
+ * Created by Amin Abu-Taleb on 27/04/2015.
  */
 public class Trie {
+
+    private Node root = new Node();
+
+    private class Node extends TreeMap<Character, Node>{
+        private Character label;
+        private boolean edge = false;
+        private String value;
+
+        public Node() {
+            super();
+        }
+
+        public Node(Character character){
+            super();
+            this.setLabel(character);
+        }
+
+        public Node search(Character character){
+            return this.get(character);
+        }
+
+        public Node insert(Character character){
+            Node n = get(character);
+            if (n == null){
+                n = new Node(character);
+                put(character, n);
+                return n;
+            }
+            else{
+                return get(character);
+            }
+        }
+
+        public Character getLabel() {
+            return label;
+        }
+
+        public void setLabel(Character label) {
+            this.label = label;
+        }
+
+        public boolean isEdge() {
+            return edge;
+        }
+
+        public void setEdge(boolean edge) {
+            this.edge = edge;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "label=" + label +
+                    ", edge=" + edge +
+                    ", value='" + value + '\'' +
+                    '}';
+        }
+    }
+
+    public void insert(String string){
+        Node node = root;
+        char[] cArray = string.toLowerCase().toCharArray();
+        for (char c: cArray){
+            node = node.insert(c);
+        }
+        node.setValue(string);
+        node.setEdge(true);
+    }
+
+    public List<String> search(String s){
+        List<String> results = new ArrayList<String>();
+        Node node = root;
+        char[] cArray = s.toLowerCase().toCharArray();
+
+        for (char c: cArray){
+            node = node.search(c);
+            if (node == null){
+                break;
+            }
+        }
+        
+        results = findWords(node);
+
+        return results;
+    }
+
+    private List<String> findWords(Node node) {
+        List<String> words = new ArrayList<String>();
+
+        if (node != null && !node.isEmpty()) {
+            Set<Map.Entry<Character, Node>> entrySet = node.entrySet();
+            for (Map.Entry<Character, Node> entry : entrySet) {
+                Node child = entry.getValue();
+                if (child.isEdge()) {
+                    words.add(child.getValue());
+                }
+                words.addAll(findWords(child));
+            }
+        }
+        return words;
+    }
 }
--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,10 +1,30 @@
+import org.abutaleb.amin.ticketmachine.algorithm.Trie;
 import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+
+import java.io.*;
 
 /**
  * Created by taleba on 27/04/2015.
  */
 public class SearchTest {
+
+    private Trie trie;
+
+    @BeforeSuite
+    public void populateData() throws IOException {
+        trie = new Trie();
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("data").getFile());
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String sCurrentLine;
+        while ((sCurrentLine = br.readLine()) != null) {
+            trie.insert(sCurrentLine.trim());
+        }
+    }
+
     @Test
     public void dummyTest(){
         Assert.assertTrue(true);
